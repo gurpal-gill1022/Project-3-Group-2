9b6d4ff20d7f3005336e8463f7688ff4547b33e4
stephfer17 <136030328+stephfer17@users.noreply.github.com>
2023-09-28 19:47:07 -0400

    Add files via upload

--- app2.py
+++ app2.py
@@ -1,6 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask,jsonify
 from pymongo import MongoClient
-
 app = Flask(__name__)
 mongo_host = 'localhost'
 mongo_port = 27017
@@ -13,17 +12,17 @@ client = MongoClient(mongo_host, mongo_port)
 db = client[mongo_db]
 
 @app.route('/')
-def index():
+def get_json():
     # Access a collection in your database
     collection = db['titles_clean']
 
     # Query the collection (e.g., find all documents)
-    documents = collection.find()
+    documents = collection.find({}, {"_id": 0})
 
     # Convert documents to a list
     data = [doc for doc in documents]
-
-    return str(data)
+    print(data)
+    return jsonify(data) 
 
 if __name__ == '__main__':
 	app.run()
